7e6d0e8471ad3fc0c1d94acc54b5d9c11636fbb7
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-03-03 17:01:47 +0100

    Set AMS, add seeds data, use api-pagination in teh controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
 
-    render json: @posts
+    paginate json: @posts
   end
 
   # GET /posts/1
--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,13 @@ module ApiPagination
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
+    end
   end
 end
